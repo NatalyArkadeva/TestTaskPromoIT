81aa72f278013d09b23096f6db31d077a50f21ca
Nataly Arkadeva <natatenok73@mail.ru>
2022-08-30 10:38:27 +0400

    Test task

--- src/main/java/HttpActions.java
+++ src/main/java/HttpActions.java
@@ -1,18 +1,21 @@
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class HttpActions {
+    private final HttpClient client;
 
-    public static String httpGetRequest(String url) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        java.net.http.HttpRequest request = java.net.http.HttpRequest.newBuilder()
+    public HttpActions() {
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String httpGetRequest(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .uri(URI.create(url))
                 .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        return response.body();
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,25 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        String string = null;
-        try {
-            string = HttpActions.httpGetRequest("http://numbersapi.com/random/trivia");
+        int number = (int) (Math.random() * 100000);
 
+        String string;
+        try {
+            string = new HttpActions().httpGetRequest("http://numbersapi.com/" + number + "/trivia");
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
+            return;
         }
-            System.out.println(string);
+        System.out.println(string);
 
-            Map<String, Integer> map = StringActions.getFrequencyMap(string);
-            System.out.println("Частоты:");
-            StringActions.printFrequencyMap(map);
+        Map<String, Integer> map = StringActions.getFrequencyMap(string);
+        System.out.println("Частоты:");
+        StringActions.printFrequencyMap(map);
 
-            System.out.printf("Среднее значение частоты %d/%d = %f\n", StringActions.sumValues(map), map.size(),
-                    (double) StringActions.sumValues(map) / map.size());
+        System.out.printf("Среднее значение частоты %d/%d = %f\n", StringActions.sumValues(map), map.size(),
+                (double) StringActions.sumValues(map) / map.size());
 
-            System.out.print("Символы, которые соответствуют условию наиболее близкого значения частоты к среднему значению: ");
-            StringActions.printClosestAverageFrequencyValue(map, (double) StringActions.sumValues(map) / map.size());
+        System.out.print("Символы, которые соответствуют условию наиболее близкого значения частоты к среднему значению: ");
+        StringActions.printClosestAverageFrequencyValue(map, (double) StringActions.sumValues(map) / map.size());
     }
 }
